cbe8bb7034fe4cefbf9536c5e8bc9751cb2faf78
Edward Tanguay (windows) <edward@tanguay.info>
2023-01-17 10:23:52 +0100

    is minimized

--- src/model.ts
+++ src/model.ts
@@ -1,18 +1,3 @@
-import fs from 'fs';
-import { IBook, IRawBook } from './interfaces.js';
-
-const rawBooks: IRawBook[] = JSON.parse(fs.readFileSync('./src/data/rawBooks.json', 'utf8'));
-
-const books: IBook[] = rawBooks.map(rawBook => {
-	return {
-		id: rawBook.id,
-		idCode: rawBook.idCode,
-		title: rawBook.title,
-		description: rawBook.description,
-		language: rawBook.language === '' ? 'english' : rawBook.language
-	}
-});
-
 export const getApiInstructions = () => {
 	return `
 <style>
@@ -28,16 +13,7 @@ export const getApiInstructions = () => {
 </style>
 <h1>Book Site API</h1>
 <ul>
-	<li><code>/books</code> - all books</li>
-	<li><code>/books/3</code> - book with id 3</li>
+	<li>nnn</li>
 </ul>
 	`;
-}
-
-export const getBooks = (): IBook[] => {
-	return books;
-}
-
-export const getBook = (id: number): IBook => {
-	return books.find(m => m.id === id);
 }
