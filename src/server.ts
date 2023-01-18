fde66c427b224b8ee056a886e053d6019f74ba50
Edward Tanguay (windows) <edward@tanguay.info>
2023-01-18 09:54:04 +0100

    basic /books in

--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,15 @@ app.get('/', (req: express.Request, res: express.Response) => {
 	res.send(model.getApiInstructions());
 });
 
+app.get('/books', async (req, res) => {
+    // const books = await Book.find();
+    // res.status(200).json({
+    //     message: 'fetched all books',
+    //     books
+    // });
+	res.send('test');
+});
+
 app.listen(config.port, () => {
 	console.log(`${config.appName} is listening on port http://localhost:${config.port}`);
 });
